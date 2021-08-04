5ef32b2114ecbd0fc8e1b423e431d3c362420656
Daniel A. Soriano II <72549944+EngrJill@users.noreply.github.com>
2021-08-04 17:37:42 +0800

    Made the design of the Para-tinda forms

--- index.php
+++ index.php
@@ -8,12 +8,14 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://cdn.jsdelivr.net/npm/vue"></script>
     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style/style.css">
 </head>
   
 <body>
-    <h1 class="h1aaaa">Para-Tinda Form</h1>
     <div id='vueapp'>
+        <div class="navbar">
+            <img src="assets/logo2.png" alt="Atud Logo White" href="">
+        </div>
 
     <!--
     <table border='1' width='100%' style='border-collapse: collapse;'>
@@ -37,45 +39,70 @@
 
     </br>
 -->
-    <form>
-        <h5 style="color: red" :style='{display: isRequired}'>All fields are required</h5>
-      <label>Location</label>
-      <select name="location" id="" v-on:change="onChangeSite($event)" v-model="location">
-         <option value="" disabled selected>Choose</option>
-         <option value="Polangui">Polangui</option>
-         <option value="Libon">Libon</option>
-         <option value="Oas">Oas</option>
-         <option value="Guinobatan">Guinobatan</option>
-         <option value="Ligao">Ligao</option>
-         <option value="Legazpi">Legazpi</option>
-         <option value="Camalig">Camalig</option>
-         <option value="Daraga">Daraga</option>
-     </select>
-      </br>
-      <label>Business Name</label>
-      <input type="text" name="business_name" v-model="business_name" class="business_name">
-      </br>
-      <label>Number of Branches/Outlets</label>
-      <input type="number" name="number_branches" v-model="number_branches" class="number_branches">
-      </br>
-      <label>Business Address</label>
-      <input type="text" name="business_address" v-model="business_address" class="business_address">
-      </br>
-      <label>Mobile Number</label>
-      <input type="text" name="mobile_number" v-model="mobile_number" class="mobile_number">
-      </br>
-      <label>Business Email</label>
-      <input type="text" name="business_email" v-model="business_email" class="business_email">
-      </br>
-      <label for="agree"><input id="agree" type="checkbox" value="agree" v-model="checked"/>I Agree</label>
-      </br>
-      <label for="agree2"><input id="agree2" type="checkbox" value="agree2" v-model="checked2"/>I Agree</label>
-      </br>
-    <input type="button" onClick="document.location.href='helloworld.php'" @click="createContact()" :disabled='isDisabled' value="Add" class="add">
-    
-    </form>
-
-</div>
+        <div class="container">
+            <div class="container-formcontainer">
+                <div class="container-formcontainer-title">
+                    <div class="img-container">
+                        <img src="assets/logo3.png" alt="Atud Logo">
+                    </div>
+                    <div class="container-formcontainer-title-bg">
+                        <h1> Para-Tinda Registration </h1>
+                    </div>
+                    <h5 style="color: red" :style='{display: isRequired}'>All fields are required</h5>
+                </div>
+                <form class="container-formcontainer-form">
+                    <label>Location</label></br>
+                    <select name="location" id="" v-on:change="onChangeSite($event)" v-model="location" class="location">
+                        <option value="" disabled selected>Choose</option>
+                        <option value="Polangui">Polangui</option>
+                        <option value="Libon">Libon</option>
+                        <option value="Oas">Oas</option>
+                        <option value="Guinobatan">Guinobatan</option>
+                        <option value="Ligao">Ligao</option>
+                        <option value="Legazpi">Legazpi</option>
+                        <option value="Camalig">Camalig</option>
+                        <option value="Daraga">Daraga</option>
+                    </select>
+                    </br>
+                    <label>Business Name</label> </br>
+                    <input type="text" name="business_name" v-model="business_name" class="business_name">
+                    </br>
+                    <div class="dual-form-container">
+                        <div class="number-branches-container">
+                            <label>Number Branches</label> </br>
+                            <input type="number" name="number_branches" v-model="number_branches" class="number_branches">
+                        </div>
+                        </br>
+                        <div class="business-address-container">
+                            <label>Business Address</label> </br>
+                            <input type="text" name="business_address" v-model="business_address" class="business_address">
+                        </div>
+                    </div>
+                    </br>
+                    <div class="number-email-container">
+                        <label>Mobile Number</label></br>
+                        <input type="text" name="mobile_number" v-model="mobile_number" class="mobile_number">
+                        </br>
+                        <label>Business Email</label></br>
+                        <input type="text" name="business_email" v-model="business_email" class="business_email">
+                        </br>
+                    </div>
+                    <label for="agree" class="agree"><input id="agree" class="agree" type="checkbox" value="agree" v-model="checked"/>By providing Atud with my personal data, I agree that Atud may collect, use and disclose
+                        my personal data for purposes in accordance with its Privacy Policy.</label></br>
+                    </br>
+                    <label for="agree2" class="agree2"><input id="agree2" class="agree2" type="checkbox" value="agree2" v-model="checked2"/>I understand that my personal data may be used for marketing purposes by Atud or its 
+                    partners; and I hereby consent to receive marketing and promotional materials by telephone, 
+                    SMS or e-mail and through other channels as determined by Atud.</label></br>
+                    </br>
+                        <input type="button" :style="{backgroundColor: isDisabledColor}" onClick="document.location.href='helloworld.php'" @click="createContact()" :disabled='isDisabled' value="REGISTER" class="add">
+                
+                </form>
+            </div>
+            <div class="container-img">
+                <img src="assets/img5.jpg" alt="Atud Para Tinda Pic">
+            </div>
+        </div>
+    </div>
 
 <script>
 var app = new Vue({
@@ -138,6 +165,28 @@ var app = new Vue({
         }
             else {
             return 'none'
+        }
+    },
+    isDisabledColor() {
+        if (this.business_name === '') {
+            return 'gray'
+        } else if (this.number_branches === '') {
+            return 'gray'
+        } else if (this.location === '') {
+            return 'gray'
+        } else if (this.business_address === '') {
+            return 'gray'
+        } else if (this.mobile_number === '') {
+            return 'gray'
+        } else if (this.business_email === '') {
+            return 'gray'
+        } else if (this.checked === false) {
+            return 'gray'
+        } else if (this.checked2 === false) {
+            return 'gray'
+        }
+            else {
+            return '#2DA81A'
         }
     }
   },
